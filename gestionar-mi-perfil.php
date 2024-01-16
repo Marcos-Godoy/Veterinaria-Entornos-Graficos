8330c526e90eee077d41d38c484c550ee90c3736
Marcos-Godoy <mjgodoy2002@gmail.com>
2024-01-16 19:30:18 -0300

    agrego los perfiles que faltaban

--- gestionar-mi-perfil.php
+++ gestionar-mi-perfil.php
@@ -15,18 +15,17 @@ $tipoUsuario = $_SESSION['tipo_usuario']; // Puede ser personal o cliente
 if ($tipoUsuario === 'cliente') {
     // Contenido para clientes
     header("Location: mi-perfil-cliente.html");
-} else{ 
-    if($tipoUsuario === 'personal' && $_SESSION['rol_id'] === 0) {
+} elseif ($tipoUsuario === 'personal') {
+    // Si es personal, verificamos el rol
+    if ($_SESSION['rol_id'] === 1) {
         // Contenido para personal (administrador)
         header("Location: mi-perfil.html");
     } else {
-        if ($tipoUsuario === 'personal' && $_SESSION['rol_id'] != 0) {
-            // Para peluqueros y veterinarios
-            header("Location: mi-perfil-personal.html");
-        } else {
-            // Tipo de usuario no reconocido, redirigir a la página de inicio
-            header("Location: pagina.html");
-            exit();
-        }
+        // Para peluqueros y veterinarios
+        header("Location: mi-perfil-personal.html");
     }
-} 
+} else {
+    // Tipo de usuario no reconocido, redirigir a la página de inicio
+    header("Location: pagina.html");
+    exit();
+}
