11a921fd91e322e2a80d7a7505d4a0ea398f64f8
Marcos-Godoy <mjgodoy2002@gmail.com>
2024-12-27 10:30:11 -0300

    Agrego validaciones en turnos

--- procesar_turno.php
+++ procesar_turno.php
@@ -13,39 +13,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $verificarPersonal = "SELECT * FROM personal WHERE id = '$personalID'";
     $resultadoVerificarPersonal = $conn->query($verificarPersonal);
 
-    if ($resultadoVerificar->num_rows > 0 && $resultadoVerificarPersonal->num_rows > 0) {
+    $verificarAdmin = "select rol_id from personal p where id = '$personalID'";
+    $resultadoVerificarAdmin = $conn->query($verificarAdmin);
+    $row = $resultadoVerificarAdmin->fetch_assoc();
 
-        $turnosActuales = "SELECT * FROM turnos WHERE fecha_hora = '$fechaHora' AND personal_id = '$personalID'";
-        $resultadoTurnos = $conn->query($turnosActuales);
-        if($resultadoTurnos->num_rows <= 0){
-            // Verificar si la fecha y hora son futuras
-        $fechaHoraActual = date('Y-m-d\TH:i');
-        if ($fechaHora > $fechaHoraActual) {
-            // Fecha y hora válidas, se puede generar el turno
-            $estado = "disponible";
-            
-            $insertarTurno = "INSERT INTO turnos (fecha_hora, servicio_id, personal_id, estado) VALUES ('$fechaHora', '$servicioID', '$personalID', '$estado')";
+    if($row['rol_id'] != 1){
+        if ($resultadoVerificar->num_rows > 0 && $resultadoVerificarPersonal->num_rows > 0) {
 
-            if ($conn->query($insertarTurno) === TRUE) {
-                // Turno generado con éxito
-                echo "<script>alert('Turno generado con éxito.'); window.location.href = 'generar_turno.html';</script>";
+            $turnosActuales = "SELECT * FROM turnos WHERE fecha_hora = '$fechaHora' AND personal_id = '$personalID'";
+            $resultadoTurnos = $conn->query($turnosActuales);
+            if($resultadoTurnos->num_rows <= 0){
+                // Verificar si la fecha y hora son futuras
+            $fechaHoraActual = date('Y-m-d\TH:i');
+            if ($fechaHora > $fechaHoraActual) {
+                // Fecha y hora válidas, se puede generar el turno
+                $estado = "disponible";
+                
+                $insertarTurno = "INSERT INTO turnos (fecha_hora, servicio_id, personal_id, estado) VALUES ('$fechaHora', '$servicioID', '$personalID', '$estado')";
+
+                if ($conn->query($insertarTurno) === TRUE) {
+                    // Turno generado con éxito
+                    echo "<script>alert('Turno generado con éxito.'); window.location.href = 'generar_turno.html';</script>";
+                } else {
+                    // Error al generar el turno
+                    echo "<script>alert('Error al generar el turno: " . $conn->error . "'); window.location.href = 'generar_turno.html';</script>";
+                }
             } else {
-                // Error al generar el turno
-                echo "<script>alert('Error al generar el turno: " . $conn->error . "'); window.location.href = 'generar_turno.html';</script>";
+                // Fecha y hora no válidas
+                echo "<script>alert('Error: La fecha y hora ingresadas deben ser futuras.'); window.location.href = 'generar_turno.html';</script>";
+            }
+            } else {
+                echo "<script>alert('Error: El personal ya tiene un turno asignado en esa fecha y hora.'); window.location.href = 'generar_turno.html';</script>";
             }
         } else {
-            // Fecha y hora no válidas
-            echo "<script>alert('Error: La fecha y hora ingresadas deben ser futuras.'); window.location.href = 'generar_turno.html';</script>";
-        }
-        } else {
-            echo "<script>alert('Error: El personal ya tiene un turno asignado en esa fecha y hora.'); window.location.href = 'generar_turno.html';</script>";
+            echo "<script>alert('Error: El servicio ID o Personal ID ingresado no existen.'); window.location.href = 'generar_turno.html';</script>";
         }
     } else {
-        echo "<script>alert('Error: El servicio ID o Personal ID ingresado no existen.'); window.location.href = 'generar_turno.html';</script>";
+        echo "<script>alert('Error: No se puede asignar un turno a un administrador.'); window.location.href = 'generar_turno.html';</script>";
     }
 }
 
-// Cerrar la conexión a la base de datos
 $conn->close();
 ?>
 
